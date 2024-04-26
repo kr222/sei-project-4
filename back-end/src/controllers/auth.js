cbd8584f0d7ec46329ff05af9e0d04c93fbb08c9
kr2 <rat.dog.girl@hotmail.com>
2024-04-26 10:12:36 +0800

    create login and refresh

--- back-end/src/controllers/auth.js
+++ back-end/src/controllers/auth.js
@@ -14,7 +14,7 @@ const getAllUsers = async (req, res) => {
 };
 
 const register = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, role } = req.body;
 
   try {
     const existingUser = await pool.query(
@@ -23,7 +23,7 @@ const register = async (req, res) => {
     );
 
     if (existingUser.rows.length > 0) {
-      return res.staus(400).json({ message: "Username already exists" });
+      return res.status(400).json({ message: "Username already exists" });
     }
     // hash pw
     const hashedPw = await bcrypt.hash(password, 12);
@@ -31,15 +31,15 @@ const register = async (req, res) => {
     const userID = uuidv4();
     // insert into db
     const newUser = await pool.query(
-      "INSERT INTO users (id,username,password_hash) VALUES ($1, $2, $3) RETURNING *",
-      [userID, username, hashedPw]
+      "INSERT INTO users (id,username,pw_hash, role) VALUES ($1, $2, $3, $4) RETURNING *",
+      [userID, username, hashedPw, role]
     );
     // generate jwt token
-    const token = jwt.sign(
-      { userID: newUser.rows[0].id, username: newUser.rows[0].username },
-      process.env.ACCESS_SECRET,
-      { expiresIn: "1h" }
-    );
+    // const token = jwt.sign(
+    //   { userID: newUser.rows[0].id, username: newUser.rows[0].username },
+    //   process.env.ACCESS_SECRET,
+    //   { expiresIn: "1h" }
+    // );
     res.status(201).json({
       token,
       userID: newUser.rows[0].id,
@@ -51,4 +51,64 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers, register };
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // check if user exists in db
+    const user = await pool.query("SELECT * FROM users WHERE username=$1", [
+      username,
+    ]);
+    if (user.rows.length === 0) {
+      console.log(`username error`);
+      return res.status(400).json({ message: "username/ password error" });
+    }
+    // check password against hash
+    const validPassword = await bcrypt.compare(password, user.rows[0].pw_hash);
+    if (!validPassword) {
+      console.log(`password error`);
+      return res.status(400).json({ message: "username/ password error" });
+    }
+    // claims and jwt
+    const claims = {
+      id: user.rows[0].id,
+      username: user.rows[0].username,
+      role: user.rows[0].role,
+    };
+
+    const access = jwt.sign(claims, process.env.ACCESS_SECRET, {
+      expiresIn: "1h",
+      jwtid: uuidv4(),
+    });
+    const refresh = jwt.sign(claims, process.env.REFRESH_SECRET, {
+      expiresIn: "30d",
+      jwtid: uuidv4(),
+    });
+
+    res.json({ access, refresh });
+  } catch (error) {
+    console.log(`problem logging in`);
+  }
+};
+
+const refresh = async (req, res) => {
+  try {
+    const decoded = jwt.verify(req.body.refresh, process.env.REFRESH_SECRET);
+    const claims = {
+      username: decoded.username,
+      role: decoded.role,
+    };
+    const access = jwt.sign(claims, process.env.ACCESS_SECRET, {
+      expiresIn: "1h",
+      jwtid: uuidv4(),
+    });
+    console.log(`${claims.role}`);
+    res.json({ access });
+  } catch (error) {
+    console.log(`refresh error`);
+    return res
+      .status(400)
+      .json({ status: "error", msg: "failed to refresh token" });
+  }
+};
+module.exports = { getAllUsers, register, login, refresh };
--- back-end/src/routers/auth.js
+++ back-end/src/routers/auth.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { getAllUsers, register } = require("../controllers/auth");
+const {
+  getAllUsers,
+  register,
+  login,
+  refresh,
+} = require("../controllers/auth");
 
 const router = express.Router();
 
 router.get("/users", getAllUsers);
 router.put("/register", register);
+router.post("/login", login);
+router.post("/refresh", refresh);
 
 module.exports = router;
