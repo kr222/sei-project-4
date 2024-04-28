e5c41879917d06be069b2ef4b04a53781a34c1a2
kr2 <rat.dog.girl@hotmail.com>
2024-04-28 21:09:28 +0800

    update calendar component

--- front-end/src/components/WorkshopCalendar.jsx
+++ front-end/src/components/WorkshopCalendar.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 import useFetch from "../hooks/useFetch";
-// import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-// import { format, parse, startOfWeek, getDay, enUS } from "date-fns";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
 
 const WorkshopCalendar = (props) => {
-  // // calendar localisation
-  // const locales = {
-  //   "en-US": enUS,
-  // };
+  // calendar localisation
+  const locales = {
+    "en-US": enUS,
+  };
 
-  // const localizer = dateFnsLocalizer({
-  //   format,
-  //   parse,
-  //   startOfWeek,
-  //   getDay,
-  //   locales,
-  // });
+  const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  });
 
-  // // store events fetched from db
+  // store events fetched from db
   const fetchData = useFetch();
   const [bookings, setBookings] = useState([]);
+
+  // state for showing/hiding calendar
+  const [showCal, setShowCal] = useState(false);
 
   const getBookings = async () => {
     try {
@@ -41,17 +46,30 @@ const WorkshopCalendar = (props) => {
     }
   };
 
+  const myEventsList = bookings.map((item) => {
+    return {
+      title: item.workshop_type,
+      start: new Date(item.booking_date),
+      end: new Date(item.booking_date),
+    };
+  });
+
   return (
     <>
       <button onClick={() => getBookings()}>dog</button>
-
-      {/* <Calendar
-        localizer={localizer}
-        events={myEventsList}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      ></Calendar> */}
+      <button onClick={() => setShowCal(true)}>show cal</button>
+      {/* <div>{bookings[0].workshop_type}</div>
+      <div>{bookings[0].booking_date}</div>
+      <div>{bookings[0].booking_cost}</div> */}
+      {showCal && (
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        ></Calendar>
+      )}
     </>
   );
 };
