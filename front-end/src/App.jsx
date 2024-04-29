d10db191a3f6e084b0a1cdeda8b545870d3755d5
kr2 <rat.dog.girl@hotmail.com>
2024-04-29 23:45:48 +0800

    add routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Route, Routes, Navigate, NavLink } from "react-router-dom";
+//components
+import NavBar from "./components/NavBar";
 import WorkshopCalendar from "./components/WorkshopCalendar";
+//pages
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div>
-      <WorkshopCalendar />
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        {/* <Route path="/" element={<Navigate replace to="/" />} /> */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </>
+    // <WorkshopCalendar />
   );
 }
 
