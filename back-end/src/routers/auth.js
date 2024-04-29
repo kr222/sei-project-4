31eab208ab1cda37089cee9c5f9acab153b42040
kr2 <rat.dog.girl@hotmail.com>
2024-04-29 23:44:59 +0800

    update auth router

--- back-end/src/routers/auth.js
+++ back-end/src/routers/auth.js
@@ -4,6 +4,8 @@ const {
   register,
   login,
   refresh,
+  editRole,
+  deleteUser,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -12,5 +14,7 @@ router.get("/users", getAllUsers);
 router.put("/register", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
+router.post("/editRole", editRole);
+router.delete("/delete", deleteUser);
 
 module.exports = router;
