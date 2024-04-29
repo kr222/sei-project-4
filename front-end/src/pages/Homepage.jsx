fe25d3f1a3cc438683d31eafb26ccde9a84776b6
kr2 <rat.dog.girl@hotmail.com>
2024-04-29 23:47:13 +0800

    create homepage.jsx

---
+++ front-end/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WorkshopCalendar from "../components/WorkshopCalendar";
+
+const Homepage = () => {
+  return (
+    <>
+      <WorkshopCalendar />
+    </>
+  );
+};
+
+export default Homepage;
