12ed67843d93315f8383036f7ad3765e418dc335
kr2 <rat.dog.girl@hotmail.com>
2024-05-01 23:03:16 +0800

    add new routers

--- back-end/src/routers/workshops.js
+++ back-end/src/routers/workshops.js
@@ -4,11 +4,15 @@ const {
   addNewBooking,
   editBooking,
   deleteBooking,
+  getWoodBookings,
+  getMetalBookings,
 } = require("../controllers/workshops");
 
 const router = express.Router();
 
 router.get("/all", getAllBookings);
+router.get("/wood", getWoodBookings);
+router.get("/metal", getMetalBookings);
 router.put("/addBooking", addNewBooking);
 router.post("/editBooking", editBooking);
 router.delete("/deleteBooking", deleteBooking);
