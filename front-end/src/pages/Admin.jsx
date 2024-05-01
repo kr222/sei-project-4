0b1cf144f7d35375287369ef94040c42a200cfa4
kr2 <rat.dog.girl@hotmail.com>
2024-05-01 23:07:15 +0800

    create admin page

---
+++ front-end/src/pages/Admin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserList from "../components/UserList";
+
+const Admin = () => {
+  return (
+    <>
+      <UserList />
+    </>
+  );
+};
+
+export default Admin;
