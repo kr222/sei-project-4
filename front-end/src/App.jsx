cbca8b0bf2ace557bc877774b0fd52b0f66e13bc
kr2 <rat.dog.girl@hotmail.com>
2024-05-01 23:03:47 +0800

    add context

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes, Navigate, NavLink } from "react-router-dom";
 //components
 import NavBar from "./components/NavBar";
@@ -7,17 +7,44 @@ import WorkshopCalendar from "./components/WorkshopCalendar";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Homepage from "./pages/Homepage";
+import UserContext from "./context/user";
+import NotFound from "./pages/NotFound";
+import Admin from "./pages/Admin";
+import Staff from "./pages/Staff";
 
 function App() {
+  const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [username, setUsername] = useState();
+  const [userId, setUserId] = useState();
+  const [role, setRole] = useState();
   return (
     <>
-      <NavBar />
-      <Routes>
-        {/* <Route path="/" element={<Navigate replace to="/" />} /> */}
-        <Route path="/" element={<Homepage />} />
-        <Route path="signin" element={<SignIn />} />
-        <Route path="signup" element={<SignUp />} />
-      </Routes>
+      <UserContext.Provider
+        value={{
+          accessToken,
+          setAccessToken,
+          refreshToken,
+          setRefreshToken,
+          username,
+          setUsername,
+          userId,
+          setUserId,
+          role,
+          setRole,
+        }}
+      >
+        <NavBar />
+        <Routes>
+          {/* <Route path="/" element={<Navigate replace to="/" />} /> */}
+          <Route path="/" element={<Homepage />} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="staff" element={<Staff />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </UserContext.Provider>
     </>
     // <WorkshopCalendar />
   );
