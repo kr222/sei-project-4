0c6dbba4aa9f595095b4d08334440767ac8f258d
kr2 <rat.dog.girl@hotmail.com>
2024-05-02 23:47:20 +0800

    update router with middleware and validators

--- back-end/src/routers/auth.js
+++ back-end/src/routers/auth.js
@@ -8,13 +8,22 @@ const {
   deleteUser,
 } = require("../controllers/auth");
 
+const { authAdmin } = require("../middleware/authMiddleware");
+
+const { errorCheck } = require("../validators/errorCheck");
+
+const {
+  validateRegistrationData,
+  validateLoginData,
+} = require("../validators/authValidator");
+
 const router = express.Router();
 
 router.get("/users", getAllUsers);
-router.put("/register", register);
-router.post("/login", login);
+router.put("/register", validateRegistrationData, errorCheck, register);
+router.post("/login", validateLoginData, errorCheck, login);
 router.post("/refresh", refresh);
-router.post("/editRole", editRole);
-router.delete("/delete", deleteUser);
+router.post("/editRole", authAdmin, editRole);
+router.delete("/delete", authAdmin, deleteUser);
 
 module.exports = router;
