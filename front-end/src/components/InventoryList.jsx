708bc15dac44c3d08ed8232413adc51d5465d340
kr2 <rat.dog.girl@hotmail.com>
2024-05-02 20:26:23 +0800

    create add material functionality

--- front-end/src/components/InventoryList.jsx
+++ front-end/src/components/InventoryList.jsx
@@ -9,14 +9,27 @@ import {
   TableHead,
   TableRow,
   TextField,
+  Select,
+  MenuItem,
+  InputAdornment,
 } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
 import InventoryListItem from "./InventoryListItem";
 
 const InventoryList = () => {
+  const userCtx = useContext(UserContext);
   const fetchData = useFetch();
   const [inventoryList, setInventoryList] = useState([]);
+  const [addMaterial, setAddMaterial] = useState(false);
+  const [newMaterial, setNewMaterial] = useState("");
+  const nameRef = useRef();
+  const quantityRef = useRef();
+
+  const handleMaterialChange = (event) => {
+    setNewMaterial(event.target.value);
+  };
 
   const getAllMaterials = async () => {
     try {
@@ -38,12 +51,84 @@ const InventoryList = () => {
     }
   };
 
+  const addNewMaterial = async () => {
+    try {
+      const res = await fetchData("/inventory/add", "PUT", {
+        material_type: newMaterial,
+        material_name: nameRef.current.value,
+        material_quantity: quantityRef.current.value,
+      });
+      if (res.ok) {
+        console.log(`material added successfully`);
+        getAllMaterials();
+      } else console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => getAllMaterials, []);
   return (
     <>
-      <Paper>
-        <Typography variant="h4">Inventory list</Typography>
-        <Button onClick={() => getAllMaterials()}>test get inventory</Button>
+      <Paper style={{ margin: "15px" }}>
+        <Typography variant="h4">Current Inventory</Typography>
+        <Button
+          onClick={() =>
+            console.log(
+              `${nameRef.current.value}, ${quantityRef.current.value}`
+            )
+          }
+        >
+          asdf
+        </Button>
+        {userCtx.role === "staff" && addMaterial && (
+          <Button
+            variant="contained"
+            color="success"
+            style={{ height: "50px", marginRight: "20px" }}
+            onClick={() => {
+              setAddMaterial(false);
+              addNewMaterial();
+            }}
+          >
+            Save
+          </Button>
+        )}
+        {userCtx.role === "staff" && !addMaterial && (
+          <Button
+            variant="contained"
+            color="success"
+            style={{ height: "50px" }}
+            onClick={() => setAddMaterial(true)}
+          >
+            Add new material
+          </Button>
+        )}
+        {addMaterial && (
+          <>
+            <Select defaultValue="select" onChange={handleMaterialChange}>
+              <MenuItem value="select">Material Type</MenuItem>
+              <MenuItem value="Wood">Wood</MenuItem>
+              <MenuItem value="Metal">Metal</MenuItem>
+            </Select>
+            <TextField
+              label="Material Name"
+              inputRef={nameRef}
+              inputProps={{ minLength: 5 }}
+            ></TextField>
+            <TextField
+              type="number"
+              label="Material Quantity"
+              inputRef={quantityRef}
+              inputProps={{ min: 1 }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">pcs</InputAdornment>
+                ),
+              }}
+            ></TextField>
+          </>
+        )}
         <TableContainer>
           <Table>
             <TableHead>
@@ -51,7 +136,9 @@ const InventoryList = () => {
                 <TableCell>Material Type</TableCell>
                 <TableCell>Material Name</TableCell>
                 <TableCell>Quantity</TableCell>
-                <TableCell>Material Id</TableCell>
+                {userCtx.role === "staff" && (
+                  <TableCell>Material Id</TableCell>
+                )}{" "}
               </TableRow>
             </TableHead>
             <TableBody>
