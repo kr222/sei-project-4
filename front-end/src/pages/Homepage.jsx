7b0108147de5d0309306bc69409514967000d4ae
kr2 <rat.dog.girl@hotmail.com>
2024-05-02 20:28:45 +0800

    tidy up code

--- front-end/src/pages/Homepage.jsx
+++ front-end/src/pages/Homepage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import WorkshopCalendar from "../components/WorkshopCalendar";
-import BookingList from "../components/BookingList";
 import InventoryList from "../components/InventoryList";
-import UserList from "../components/UserList";
 
 const Homepage = () => {
   return (
