cf8a24981e4bbba2d6af30d76bf7d629dca07a96
kr2 <rat.dog.girl@hotmail.com>
2024-05-02 20:25:51 +0800

    tidy up code

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Route, Routes, Navigate, NavLink } from "react-router-dom";
 //components
 import NavBar from "./components/NavBar";
-import WorkshopCalendar from "./components/WorkshopCalendar";
+
 //pages
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -36,7 +36,6 @@ function App() {
       >
         <NavBar />
         <Routes>
-          {/* <Route path="/" element={<Navigate replace to="/" />} /> */}
           <Route path="/" element={<Homepage />} />
           <Route path="signin" element={<SignIn />} />
           <Route path="signup" element={<SignUp />} />
@@ -46,7 +45,6 @@ function App() {
         </Routes>
       </UserContext.Provider>
     </>
-    // <WorkshopCalendar />
   );
 }
 
