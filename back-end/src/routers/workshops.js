6ca7c2c46caa485ff32a25cfb83793cfa7263620
kr2 <rat.dog.girl@hotmail.com>
2024-05-02 23:48:28 +0800

    update router with middleware

--- back-end/src/routers/workshops.js
+++ back-end/src/routers/workshops.js
@@ -8,13 +8,15 @@ const {
   getMetalBookings,
 } = require("../controllers/workshops");
 
+const { authUser, authStaff } = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 router.get("/all", getAllBookings);
 router.get("/wood", getWoodBookings);
 router.get("/metal", getMetalBookings);
-router.put("/addBooking", addNewBooking);
-router.post("/editBooking", editBooking);
-router.delete("/deleteBooking", deleteBooking);
+router.put("/addBooking", authUser, addNewBooking);
+router.post("/editBooking", authStaff, editBooking);
+router.delete("/deleteBooking", authStaff, deleteBooking);
 
 module.exports = router;
