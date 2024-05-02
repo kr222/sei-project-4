91fd48f0a0c291b6957412f07a2e66e1ff7120e1
kr2 <rat.dog.girl@hotmail.com>
2024-05-02 23:47:48 +0800

    update router with middleware and validators

--- back-end/src/routers/inventory.js
+++ back-end/src/routers/inventory.js
@@ -6,11 +6,33 @@ const {
   deleteMaterial,
 } = require("../controllers/inventory");
 
+const { authStaff } = require("../middleware/authMiddleware");
+
+const { errorCheck } = require("../validators/errorCheck");
+
+const {
+  validateMaterialName,
+  validateMaterialQuantity,
+} = require("../validators/inventoryValidator");
+
 const router = express.Router();
 
 router.get("/materials", getAllMaterials);
-router.put("/add", addNewMaterial);
-router.post("/edit", editMaterial);
-router.delete("/delete", deleteMaterial);
+router.put(
+  "/add",
+  authStaff,
+  validateMaterialName,
+  validateMaterialQuantity,
+  errorCheck,
+  addNewMaterial
+);
+router.post(
+  "/edit",
+  authStaff,
+  validateMaterialQuantity,
+  errorCheck,
+  editMaterial
+);
+router.delete("/delete", authStaff, deleteMaterial);
 
 module.exports = router;
